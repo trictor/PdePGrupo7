cce89dfbc1e5187de7bc85f4afac27944d20da71
Unknown <rodrigogastonperez15@gmail.com>
2018-04-10 14:09:34 -0300

    Puse los test al inicio de todo
    Solamente se cambió la ubicación de los test.

--- TP_Entrega_1.hs
+++ TP_Entrega_1.hs
@@ -4,6 +4,43 @@ import Data.List
 import Data.Maybe
 import Test.Hspec
 
+--TESTS--
+testear = hspec $ do
+	test1
+	test2
+	test3
+	test4
+	test5
+	test6
+	test7
+	test8
+	test9
+	test10
+
+test1 = it "La billetera deberia quedar en 20, luego de depositar 10" ( ( billetera.(deposito 10) $ pepe )`shouldBe` 20)
+
+test2 = it "La billetera deberia quedar en 7, luego de extraer 3" ( ( billetera.(extracción 3) $ pepe )`shouldBe` 7)
+
+test3 = it "La billetera deberia quedar en 0, luego de extraer 15" ( ( billetera.(extracción 15) $ pepe )`shouldBe` 0)
+
+test4 = it "La billetera deberia quedar en 12, luego de un upgrade" ( ( billetera.upgrade $ pepe )`shouldBe` 12)
+
+test5 = it "La billetera deberia quedar en 0, luego de cerrar cuenta" ( ( billetera.cierreDeCuenta $ pepe )`shouldBe` 0)
+
+test6 = it "La billetera deberia quedar en 10, si no se hace nada" ( ( billetera.quedaIgual $ pepe )`shouldBe` 10)
+
+test7 = it "La billetera deberia quedar en 1020, luego de depositar 1000 y hacer un upgrade" ( ( billetera.upgrade.(deposito 1000) $ pepe )`shouldBe` 1020)
+
+test8 = it "La billetera de pepe esta en 10" (billetera pepe `shouldBe` 10)
+
+test9 = it "la Billetera de Pepe queda en 0, luego de un cierre de cuenta" (( billetera.cierreDeCuenta $ pepe ) `shouldBe` 0)
+
+test10 = it "La billetera deberia quedar en 27.6, luego de depositar 15, extraer 2 y hacer un upgrade" ( ( billetera.upgrade.(extracción 2).(deposito 15) $ pepe )`shouldBe` 27.6)
+
+{-
+	testN = it "QueTestea" (EstoEjecuta `shouldBe` Resultado)
+-}
+
 data Usuario= UnUsuario {
         nombre :: String,
         billetera::Dinero
@@ -15,7 +52,7 @@ type Dinero = Float
 pepe = UnUsuario "Jose" 10
 lucho = UnUsuario "Luciano" 2
 
--- 1) EVENTOS 
+-- 1) EVENTOS
 
 deposito :: Dinero -> Usuario -> Usuario
 
@@ -45,50 +82,5 @@ cierreDeCuenta :: Usuario -> Usuario
 
 cierreDeCuenta unUsuario = unUsuario {billetera = 0}
 
-quedaIgual :: Usuario -> Usuario 
+quedaIgual :: Usuario -> Usuario
 quedaIgual = id
-
-
-
---TESTS
-
-
-testear = hspec $ do
-	test1
-	test2
-	test3
-	test4
-	test5
-	test6
-	test7
-	test8
-	test9
-	test10
-
-test1 = it "La billetera deberia quedar en 20, luego de depositar 10" ( ( billetera.(deposito 10) $ pepe )`shouldBe` 20)
-
-test2 = it "La billetera deberia quedar en 7, luego de extraer 3" ( ( billetera.(extracción 3) $ pepe )`shouldBe` 7)
-
-test3 = it "La billetera deberia quedar en 0, luego de extraer 15" ( ( billetera.(extracción 15) $ pepe )`shouldBe` 0)
-
-test4 = it "La billetera deberia quedar en 12, luego de un upgrade" ( ( billetera.upgrade $ pepe )`shouldBe` 12)
-
-test5 = it "La billetera deberia quedar en 0, luego de cerrar cuenta" ( ( billetera.cierreDeCuenta $ pepe )`shouldBe` 0)
-
-test6 = it "La billetera deberia quedar en 10, si no se hace nada" ( ( billetera.quedaIgual $ pepe )`shouldBe` 10)
-
-test7 = it "La billetera deberia quedar en 1020, luego de depositar 1000 y hacer un upgrade" ( ( billetera.upgrade.(deposito 1000) $ pepe )`shouldBe` 1020)
-
-
-test8 = it "La billetera de pepe esta en 10" (billetera pepe `shouldBe` 10)
-
-test9 = it "la Billetera de Pepe queda en 0, luego de un cierre de cuenta" (( billetera.cierreDeCuenta $ pepe ) `shouldBe` 0)
-
-test10 = it "La billetera deberia quedar en 27.6, luego de depositar 15, extraer 2 y hacer un upgrade" ( ( billetera.upgrade.(extracción 2).(deposito 15) $ pepe )`shouldBe` 27.6)
-
-
-{-
-	testN = it "QueTestea" (EstoEjecuta `shouldBe` Resultado)
-
-
--}
