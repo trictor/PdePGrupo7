fc2c465a769c8b88366c932b14f518afe7b6d7d9
SiSoyYoFacu <dominguezfacundonicolas96@gmail.com>
2018-04-10 09:25:34 -0300

    Agregue los tipos

--- TP Entrega 1.hs
+++ TP Entrega 1.hs
@@ -6,31 +6,46 @@ import Test.Hspec
 
 data Usuario= UnUsuario {
         nombre :: String,
-        billetera::Float
+        billetera::Dinero
       } deriving (Show, Eq)
+
+type Dinero = Float
 
 --EJEMPLOS--
 pepe = UnUsuario "Jose" 10
 lucho = UnUsuario "Luciano" 2
 
--- 1) EVENTOS
+-- 1) EVENTOS 
+
+deposito :: Dinero -> Usuario -> Usuario
 
 deposito dineroADepositar usuario = usuario{ billetera = dineroADepositar + (billetera usuario)}
+
+extracción :: Dinero -> Usuario -> Usuario
 
 extracción dineroAExtraer usuario =
         usuario{ billetera = (resultadoFinal ((billetera usuario) - dineroAExtraer)) }
 
+resultadoFinal :: Dinero -> Dinero
+
 resultadoFinal dineroRestado | dineroRestado  > 0 = dineroRestado
                              | otherwise          = 0
 
+upgrade :: Usuario -> Usuario
+
 upgrade usuario = usuario{ billetera = upgradeBilletera (billetera usuario)}
+
+upgradeBilletera :: Dinero -> Dinero
 
 upgradeBilletera monto | monto * 0.2 < 10 = monto * 1.2
                        | otherwise        = monto + 10
 
+
+cierreDeCuenta :: Usuario -> Usuario
+
 cierreDeCuenta unUsuario = unUsuario {billetera = 0}
 
-
+quedaIgual :: Usuario -> Usuario 
 quedaIgual = id
 
 
